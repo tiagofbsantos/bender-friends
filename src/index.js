fe52fc2026581a160e3d6f5b16008aa4f392dc9c
tiagofbsantos <tiago.filipe.bz.santos@gmail.com>
2019-11-06 22:27:20 +0000

    Created a new CardList component that generates all the Card components

--- src/index.js
+++ src/index.js
@@ -1,18 +1,14 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Card from './Card';
+import CardList from './CardList';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
 import { robots } from './robots';
 
 //Card is the name of the component
 ReactDOM.render(
-	<Fragment>
-		<Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>
-		<Card id={robots[1].id} name={robots[1].name} email={robots[1].email}/>
-		<Card id={robots[2].id} name={robots[2].name} email={robots[2].email}/>
-	</Fragment>
+		<CardList robots={robots}/>
 	, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
